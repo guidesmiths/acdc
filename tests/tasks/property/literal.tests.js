8aa3374465eeeeb08551cba900034757e398bd6e
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2015-09-01 00:12:33 +0100

    Literal task

--- tests/tasks/property/literal.tests.js
+++ tests/tasks/property/literal.tests.js
@@ -2,36 +2,36 @@ var assert = require('assert')
 var flow = require('../../../lib/tasks/flow')
 var property = require('../../../lib/tasks/property')
 
-describe('Property Copy', function() {
+describe('Property Literal', function() {
 
-    it('should require a from path', function(done) {
-        copy({}, { from: 1 }, function(err) {
+    it('should require a value parameter', function(done) {
+        literal(undefined, {}, function(err) {
             assert.ok(err)
-            assert.equal(err.message, 'child "params" fails because [child "from" fails because ["from" must be a string]]')
+            assert.equal(err.message, 'child "params" fails because [child "value" fails because ["value" is required]]')
             done()
         })
     })
 
     it('should require a to path', function(done) {
-        copy({}, { from: 'a.b.c', to: 1 }, function(err) {
+        literal(undefined, { value: 3, to: 1 }, function(err) {
             assert.ok(err)
             assert.equal(err.message, 'child "params" fails because [child "to" fails because ["to" must be a string]]')
             done()
         })
     })
 
-    it('should copy the value from the source path to the destination path', function(done) {
-        copy({ a: { b: 2 } }, { from: 'a.b', to: 'x.y' }, function(err, result) {
+    it('should set the value to the destination path', function(done) {
+        literal(undefined, { value: 3, to: 'x.y' }, function(err, result) {
             assert.ifError(err)
-            assert.strictEqual(result.x.y, 2)
+            assert.strictEqual(result.x.y, 3)
             done()
         })
     })
 
-    function copy(input, params, cb) {
+    function literal(input, params, cb) {
         flow.run.fn(input, {
             params: {
-                task: property.copy,
+                task: property.literal,
                 params: params
             }
         }, cb)
