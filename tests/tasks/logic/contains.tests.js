67109f78a4e56cc60ef92a1bade781e50524f1b7
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2015-09-02 22:02:03 +0100

    Adding contains task

--- tests/tasks/logic/contains.tests.js
+++ tests/tasks/logic/contains.tests.js
@@ -2,36 +2,44 @@ var assert = require('assert')
 var flow = require('../../../lib/tasks/flow')
 var logic = require('../../../lib/tasks/logic')
 
-describe('eq', function() {
+describe('contains', function() {
+
+    it('should require input to be an array param', function(done) {
+        contains(1, {}, function(err) {
+            assert.ok(err)
+            assert.equal(err.message, 'child "input" fails because ["input" must be an array]')
+            done()
+        })
+    })
 
     it('should require a value param', function(done) {
-        eq(undefined, {}, function(err) {
+        contains([], {}, function(err) {
             assert.ok(err)
             assert.equal(err.message, 'child "params" fails because [child "value" fails because ["value" is required]]')
             done()
         })
     })
 
-    it('should yield true when input is strictly equal to value', function(done) {
-        eq(1, { value: 1 }, function(err, result) {
+    it('should yield true when input contains value', function(done) {
+        contains([0, 1, 3], { value: 1 }, function(err, result) {
             assert.ifError(err)
             assert.strictEqual(result, true)
             done()
         })
     })
 
-    it('should yield false when input is not strictly equal to value', function(done) {
-        eq(1, { value: '1' }, function(err, result) {
+    it('should yield false when input does not contain value', function(done) {
+        contains([0, 2, 3], { value: 4 }, function(err, result) {
             assert.ifError(err)
             assert.strictEqual(result, false)
             done()
         })
     })
 
-    function eq(input, params, cb) {
+    function contains(input, params, cb) {
         flow.run.fn(input, {
             params: {
-                task: logic.eq,
+                task: logic.contains,
                 params: params
             }
         }, cb)
